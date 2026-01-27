eaa1932df1134b25505abccda63eef5a8ee9e886
aswins33 <aswinsivan28@gmail.com>
2026-01-27 12:42:30 +0530

    if_else_statement.py

---
+++ if_else_statement.py
@@ -0,0 +1,10 @@
+# ask user what is the prize of car?
+# if prize of car less than 20 lakhs = can buy car else can't buy
+
+car= int(input("what is your car prize:"))
+if car <20:
+    print("you can buy the car")
+    if <car:
+        print("you can buy the car with your cash")
+    else:
+        print("you cannot buy the car")
