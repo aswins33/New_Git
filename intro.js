9a5308afaea33839c57d5855f5f85208f4636428
aswins33 <146815534+aswins33@users.noreply.github.com>
2026-04-06 13:49:43 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,17 @@
+console.log("hello Developers")
+
+let name = "aswin"
+
+let age = 23
+
+console.log(`my name is ${name} and i am ${age} year old`);
+
+
+let age = 18 
+
+if(age>=23){
+    console.log("Eligible to vote");
+}
+else{
+    console.log("Note eligible to vote");
+}
